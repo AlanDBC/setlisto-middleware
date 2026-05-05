e45361ff330d47e5a63726761db230dd5c5cd3bb
AlanB <AlanB@10.63.11.76>
2026-05-05 16:25:13 +0200

    Prueba de cambio para commit y push

--- src/com/setlisto/model/AbstractValueObject.java
+++ src/com/setlisto/model/AbstractValueObject.java
@@ -6,7 +6,7 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
  * Clase base para los objetos de valor (Value Objects) en el modelo.
  * Proporciona una implementación predeterminada del método toString()
  * utilizando la reflexión para generar una representación en cadena
- * de los atributos del objeto.
+ * de los atributos del objeto. Comun.
  */
 public abstract class AbstractValueObject {
 
